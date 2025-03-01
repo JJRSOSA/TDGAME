3af72143f9d14aaf7f5c019aca54328589ba8085
JJRSOSA <158442649+JJRSOSA@users.noreply.github.com>
2025-03-01 14:07:22 +0500

    project start

---
+++ Assets/Turret.cs
@@ -0,0 +1,12 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform 
+
+    [Header("Attribute")]
+    [SerializeField] private float targetingRange = 5f;
+
+}
